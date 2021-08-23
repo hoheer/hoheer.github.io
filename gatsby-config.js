fe0de55ad6601a9400e708c75599d7b6af615069
siyeon lee <temperlooser@gmail.com>
2021-08-23 10:40:09 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,8 @@ module.exports = {
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'zpzg soft',
-        description: 's-dot system operator, jun 2021 - Present',
+        name: '서울시 도시데이터 팀',
+        description: '지피지기소프트 인턴, jun 2021 - Present, 주요 업무: 도시데이터 활용 분석 리포트 작성, 인포그래픽 제작, DBMS 및 시스템 장애 처리, 데이터 관련 민원업무 처리',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
